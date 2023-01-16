008c0757dd3339291e9094333d825ce1ec548424
GitDevla <pataki.david.ferenc@gmail.com>
2023-01-16 14:41:44 +0100

    Final version

---
+++ Hazi-ImageBox/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Hazi_ImageBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.button_generate = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Location = new System.Drawing.Point(13, 26);
+            this.numericUpDown.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(180, 26);
+            this.numericUpDown.TabIndex = 0;
+            this.numericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button_generate
+            // 
+            this.button_generate.Location = new System.Drawing.Point(217, 21);
+            this.button_generate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button_generate.Name = "button_generate";
+            this.button_generate.Size = new System.Drawing.Size(112, 35);
+            this.button_generate.TabIndex = 1;
+            this.button_generate.Text = "Generálás";
+            this.button_generate.UseVisualStyleBackColor = true;
+            this.button_generate.Click += new System.EventHandler(this.button_generate_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 155);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(745, 215);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 370);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button_generate);
+            this.Controls.Add(this.numericUpDown);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+        private System.Windows.Forms.Button button_generate;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ Hazi-ImageBox/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hazi_ImageBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int imageSize = 100;
+        private void button_generate_Click(object sender, EventArgs e)
+        {
+            int numberOfPics = (int)numericUpDown.Value;
+            flowLayoutPanel1.Controls.Clear();
+            for (int i = 0; i < numberOfPics; i++)
+            {
+                var pictureBox = new PictureBox();
+                flowLayoutPanel1.Controls.Add(pictureBox);
+                pictureBox.Size = new Size(imageSize, imageSize);
+                pictureBox.BorderStyle = BorderStyle.FixedSingle;
+                pictureBox.Click += onClickEvent;
+            }
+        }
+
+        private void onClickEvent(object sender, EventArgs e)
+        {
+            var pic = (PictureBox)sender;
+            var images = Directory.GetFiles("imgs");
+            images = images.Where(i => i != pic.ImageLocation).ToArray();
+            var rand = new Random();
+            var randomImage = images[rand.Next(0, images.Length)];
+            pic.ImageLocation = randomImage;
+            pic.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+    }
+}
